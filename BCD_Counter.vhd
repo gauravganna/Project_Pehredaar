c1b17400a23b70cfd7449b3ded7d0b5778bd38e9
Gaurav G <clear>
2018-03-29 21:27:42 +0530

    Few changes

--- BCD_Counter.vhd
+++ BCD_Counter.vhd
@@ -1,4 +1,4 @@
---VHDL code for D_FlipFlop created by Gaurav Ganna as part of Project Pehredar
+--VHDL code for D_FlipFlop created by Gaurav Ganna as part of Project PEHREDAR.
 --Date 28/03/2018
 library ieee;							-- Library Declaration
 use ieee.std_logic_1164.all;		-- Use std_logic_1164 package from ieee library
@@ -9,7 +9,7 @@ entity BCD_Counter is				-- Entity Declaration
 		CLK : in std_logic;										-- Main clock
 		EN  : in std_logic;										-- Active Low Enable
 		Q   : buffer std_logic_vector (3 downto 0);		-- Output Vector (BCD Number)
-		C   : inout std_logic);									-- Carry Indicator
+		C   : inout std_logic);									-- Active Low Carry Indicator
 end BCD_Counter;
 
 architecture STRUCT of BCD_Counter is 		
@@ -22,6 +22,6 @@ begin
 	U3 : D_FlipFlop port map(S2,EN_F,S1,Q(2),S2);				--Q2
 	U4 : D_FlipFlop port map(S3,EN_F,S2,Q(3),S3);				--Q3
 	
-	C <= (not Q(3)) or Q(2) or (not Q(1)) or Q(0);		-- Combinational Logic for Carry
+	C <= (not Q(3)) or Q(2) or (not Q(1)) or Q(0);		-- Combinational Logic for Carry 
 	EN_F <= C and EN;												-- Final Output
 end STRUCT;
