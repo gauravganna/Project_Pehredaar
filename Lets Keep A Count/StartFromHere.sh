9e9afd3bcc1d051fb163baae96c4c3fa3910227d
Gaurav G <clear>
2018-04-21 13:18:35 +0530

    Creating Report of the Project

--- Lets Keep A Count/StartFromHere.sh
+++ Lets Keep A Count/StartFromHere.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-
+> entry.txt
+> exit.txt
 #It will be used to determine whether the file has modified from the previous time snap.
-echo "Lecture Started"
+echo "Attendance Started | Class to start soon"
 LAST=`ls -l Attendance_Sheet.txt`											
 x=1
+before=0
+sleep 10
 while  [ $x -le 4 ]												#Will mark entries for total of 2 minutes at the start time of lecture.
 do
-	before="$(wc -l < Attendance_Sheet.txt)"						#Store the number of lines in the file before any addition.
-	sleep 5														#Wait for 10 second
+	echo "Loop Number "$x""												#Wait for 10 second
   	NEW=`ls -l Attendance_Sheet.txt`								#Data to determine whether the file has changed from the previous time snapshot.
   	if [ "$NEW" != "$LAST" ]; then									#If file has changed.
     	my_output="$(awk '!seen[$0]++' Attendance_Sheet.txt)"		#Display all the lines from the original file removing all the duplicates (not just the consecutive ones), while maintaining the original order of statements in the file.
@@ -21,28 +23,33 @@ do
 			oldcount="$(./Dec2Bin_Converter "$before")"													#converting the old count to it's corresponding BCD Number.
 			sed -i '54s/.*/				O <= "'"${oldcount}"'";/' Haziri.vhd							#Updates the old count in the vhdl program.
 			
+			before="$(wc -l < Attendance_Sheet.txt)"						#Store the number of lines in the file before any addition.
 			./RUN.sh 												#Compile the VHDL Project and upload it to the CPLD Board.
 			echo "Successfully Updated"
 		fi
 	LAST="$NEW"
+  	else
+  		sleep 25
   	fi
   	x=$(( $x + 1 ))													#+10 seconds. Increments x by 1
 done
-
+echo "Class Started | No LateComers!!"
 cp Attendance_Sheet.txt entry.txt									#Rename Attendance Sheet as entry Sheet.
 > Attendance_Sheet.txt												#Create new Attendance Sheet to record exit entries.								
-#Doesn't Accept any new entries for the next 30 seconds.
 
+
+#Doesn't Accept any new entries for the next 30 seconds.
 sleep 30											#Sleeps for 30 seconds(Lecture hour). Late Comers aren't allowed!!!. 
 
 #Attendance is again taken when Class gets finished. :) To Remove Proxys. :(
-echo "Lecture Over"
+echo "Lecture Over | Outgoing Attendance Started"
 LAST=`ls -l Attendance_Sheet.txt`											
 x=1
-while  [ $x -le 12 ]											#Will mark entries for total of 2 minutes at the end time of lecture.
+before=0
+sleep 10
+while  [ $x -le 4 ]											#Will mark entries for total of 2 minutes at the end time of lecture.
 do
-	before="$(wc -l < Attendance_Sheet.txt)"					#Store the number of lines in the file before any addition.
-	sleep 10													#Wait for 10 second
+	echo "Loop Number "$x""
   	NEW=`ls -l Attendance_Sheet.txt`							#Data to determine whether the file has changed from the previous time snapshot.
   	if [ "$NEW" != "$LAST" ]; then								#If file has changed.
     	my_output="$(awk '!seen[$0]++' Attendance_Sheet.txt)"	#Display all the lines from the original file removing all the duplicates (not just the consecutive ones), while maintaining the original order of statements in the file.
@@ -55,13 +62,19 @@ do
 			oldcount="$(./Dec2Bin_Converter "$before")"												#converting the old count to BCD
 			sed -i '54s/.*/				O <= "'"${oldcount}"'";/' Haziri.vhd						#Updates the old count in the vhdl program.
 			
+			before="$(wc -l < Attendance_Sheet.txt)"					#Store the number of lines in the file before any addition.													#Wait for 10 second
 			./RUN.sh 											#Compile the VHDL Project and upload it to the CPLD Board.
 			echo "Successfully Updated"
 		fi
 	LAST="$NEW"
+  	else
+  		sleep 25
   	fi
   	x=$(( $x + 1 ))												#+10 seconds.Increment x by 1
 done
+echo "Outgoing Attendance Over | Further Grievances if any, will be entertained in the Dean's Office!!!"
+cp Attendance_Sheet.txt exit.txt								#Rename Attendance Sheet as exit sheet.
+> Attendance_Sheet.txt
 
-mv Attendance_Sheet.txt exit.txt								#Rename Attendance Sheet as exit sheet.
-
+sed -i '48s/.*/		if(RISING_EDGE(CLK_NEW) and cnt <= 0) then/' Haziri.vhd	#change the vhdl program according to the new ID's added
+sed -i '54s/.*/				O <= "0000000000000000";/' Haziri.vhd							#Updates the old count in the vhdl program.
